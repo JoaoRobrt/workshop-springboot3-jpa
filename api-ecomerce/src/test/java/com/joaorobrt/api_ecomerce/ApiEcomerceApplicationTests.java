2dfa7951925dbe3cbd72693ba153cb5c608f1c55
João Roberto Galvão de Oliveira <joaorobertogalvao.2008@gmail.com>
2025-04-01 15:29:49 -0300

    Project created

---
+++ api-ecomerce/src/test/java/com/joaorobrt/api_ecomerce/ApiEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaorobrt.api_ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

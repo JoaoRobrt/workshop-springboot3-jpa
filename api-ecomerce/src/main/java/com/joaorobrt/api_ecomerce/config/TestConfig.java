c17937cdb757275323040ffbf0f6d2095a3e07b9
João Roberto Galvão de Oliveira <joaorobertogalvao.2008@gmail.com>
2025-04-03 11:02:36 -0300

    Category

--- api-ecomerce/src/main/java/com/joaorobrt/api_ecomerce/config/TestConfig.java
+++ api-ecomerce/src/main/java/com/joaorobrt/api_ecomerce/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.joaorobrt.api_ecomerce.entities.Category;
 import com.joaorobrt.api_ecomerce.entities.Order;
 import com.joaorobrt.api_ecomerce.entities.User;
 import com.joaorobrt.api_ecomerce.entities.enums.OrderStatus;
+import com.joaorobrt.api_ecomerce.repositories.CategoryRepository;
 import com.joaorobrt.api_ecomerce.repositories.OrderRepository;
 import com.joaorobrt.api_ecomerce.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		User u1 = new User(null, "Bianca Meira", "biancavmeiraa@gmail.com", "985476932", "123456");
 		User u2 = new User(null, "João Roberto", "joaorobertogalvao.2008@gmail.com", "985468741", "123456");
@@ -34,8 +45,11 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
 		
 	}
 	
---
+++ api-ecomerce/src/main/java/com/joaorobrt/api_ecomerce/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.joaorobrt.api_ecomerce.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaorobrt.api_ecomerce.entities.Category;
+import com.joaorobrt.api_ecomerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
